53448b20fa0f0c7cc85927ab33d997bea3d3d080
danis <ahrnadeev@gmail.com>
2024-05-26 02:03:12 +0300

    начало++

--- src/main/java/runnable/MainController.java
+++ src/main/java/runnable/MainController.java
@@ -1,27 +1,79 @@
 package runnable;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.Label;
-import javafx.scene.control.SplitPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.*;
 
 import java.net.URL;
+import java.util.Arrays;
 import java.util.ResourceBundle;
+
+import static java.util.Objects.isNull;
 
 public class MainController implements Initializable {
     @FXML
     private Label welcomeText;
+
     @FXML
-    public LineChart<Number, Number> plot;
+    private TextField textFieldOne;
+    @FXML
+    private TextField textFieldTwo;
+    @FXML
+    private Button submitButton;
+    @FXML
+    private LineChart<Number, Number> plot;
 /*    @FXML
     public SplitPane splitPane;*/
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected double[][] handleSubmitEvent(ActionEvent event) {
+        if (event.getSource() != submitButton) return null;
+
+        String[] xText = textFieldOne.getText().split("\\s+");
+        String[] yText = textFieldTwo.getText().split("\\s+");
+
+        System.out.println(Arrays.toString(xText));
+        System.out.println(Arrays.toString(yText));
+
+        if (xText.length != yText.length) return null;
+
+        int size = xText.length;
+        double[] x = new double[size];
+        double[] y = new double[size];;
+
+        for(int i = 0; i < size; i++) {
+            xText[i] = validateNumber(xText[i]);
+            yText[i] = validateNumber(yText[i]);
+
+            if (!(isNull(xText[i]) || isNull(yText[i]))) {
+                x[i] = Double.parseDouble(xText[i]);
+                y[i] = Double.parseDouble(yText[i]);
+            } else {
+                return null;
+            }
+        }
+
+        System.out.println(Arrays.toString(x));
+        System.out.println(Arrays.toString(y));
+
+        return new double[][]{x, y};
+    }
+
+    private String validateNumber(String text) {
+        text = text.replace(",", ".");
+        if (text.matches("[+-]?([0-9]*[.])?[0-9]+")) {
+            return text;
+        } else {
+            return null;
+        }
     }
 
     @Override
@@ -36,7 +88,7 @@ public class MainController implements Initializable {
         plot.setCreateSymbols(false);
         plot.getData().add(seriesOne);
 
-/*        Label label = new Label("я лох");
+/*        Label label = new Label("нет");
         StackPane stackPane = (StackPane) splitPane.getItems().get(1);
         stackPane.getChildren().add(label);*/
     }
