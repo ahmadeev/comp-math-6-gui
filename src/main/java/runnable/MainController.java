305f3fe436f3fe9b32c9ad746498802650b2f2b7
danis <ahrnadeev@gmail.com>
2024-05-27 05:50:22 +0300

    вау вау вау

--- src/main/java/runnable/MainController.java
+++ src/main/java/runnable/MainController.java
@@ -34,28 +34,35 @@ public class MainController implements Initializable {
 
         double[][] result = handleTextInput(event);
         if (!isNull(result)) {
-            double[] x = result[0];
-            double[] y = result[1];
 
-            LinearData linearData = new LinearData(x, y);
-            System.out.println(linearData.toString());
-            QuadraticData quadraticData = new QuadraticData(x, y);
-            System.out.println(quadraticData.toString());
-            CubicData cubicData = new CubicData(x, y);
-            System.out.println(cubicData.toString());
-            ExponentialData exponentialData = new ExponentialData(x, y);
-            System.out.println(exponentialData.toString());
-            LogarithmicData logarithmicData = new LogarithmicData(x, y);
-            System.out.println(logarithmicData.toString());
-            PowerFunctionData powerFunctionData = new PowerFunctionData(x, y);
-            System.out.println(powerFunctionData.toString());
+            if (result[0].length < 8 || result[0].length > 12) {
+                showAlert(Alert.AlertType.ERROR, "error!", "Введенные данные должны содержать от восьми до двенадцати точек!");
+                textFieldOne.setText("");
+                textFieldTwo.setText("");
+            } else {
+                double[] x = result[0];
+                double[] y = result[1];
 
-            //drawDots(result);
-            for(int i = 1; i <= 6; i++) {
-                drawLine(i, result);
+                LinearData linearData = new LinearData(x, y);
+                System.out.println(linearData.toString());
+                QuadraticData quadraticData = new QuadraticData(x, y);
+                System.out.println(quadraticData.toString());
+                CubicData cubicData = new CubicData(x, y);
+                System.out.println(cubicData.toString());
+                ExponentialData exponentialData = new ExponentialData(x, y);
+                System.out.println(exponentialData.toString());
+                LogarithmicData logarithmicData = new LogarithmicData(x, y);
+                System.out.println(logarithmicData.toString());
+                PowerFunctionData powerFunctionData = new PowerFunctionData(x, y);
+                System.out.println(powerFunctionData.toString());
+
+                //drawDots(result);
+                for(int i = 1; i <= 6; i++) {
+                    drawLine(i, result);
+                }
             }
         } else {
-            showAlert(Alert.AlertType.ERROR, "error!", "Ошибка ввода!");
+            showAlert(Alert.AlertType.ERROR, "error!", "Введены некорректные данные!");
             textFieldOne.setText("");
             textFieldTwo.setText("");
         }
